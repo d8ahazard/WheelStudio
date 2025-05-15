a407407d74e83dc2f31a81c6b92817e782569e23
d8ahazard <d8ahazard@gmail.com>
2025-05-15 10:26:07 -0500

    Why??

--- manage_submodules.py
+++ manage_submodules.py
@@ -12,10 +12,11 @@ SUBMODULES = {
     "mamba": "https://github.com/d8ahazard/mamba.git",
     "causal-conv1d": "https://github.com/d8ahazard/causal-conv1d.git",
     "fairseq": "https://github.com/d8ahazard/fairseq.git",
-    "stable-audio-tools": "https://github.com/d8ahazard/stable-audio-tools.git",
+    "stable-audio-tool": "https://github.com/d8ahazard/stable-audio-tools.git",
     "versatile_audio_super_resolution": "https://github.com/d8ahazard/versatile_audio_super_resolution.git",
     "openvoice-cli": "https://github.com/d8ahazard/openvoice-cli.git",
     "CLAP": "https://github.com/d8ahazard/CLAP.git",
+    "dctorch": "https://github.com/d8ahazard/dctorch.git",
 }
 
 def run_command(cmd, cwd=None):
